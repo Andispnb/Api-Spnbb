4d50a8c08095ac8538c652b9b08ad4a3520214a4
Andispnb <77096268+Andispnb@users.noreply.github.com>
2021-04-05 21:55:12 +0800

    api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')
 }
 
-var creatorList = ['@anker_2412'];
+var creatorList = ['@AndiSpnb'];
 var creator = creatorList[Math.floor(Math.random() * creatorList.length)];
 
 
